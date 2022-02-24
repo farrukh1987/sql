d82f5cda57f4480784a7b8ee5930b16d41156330
farrukh1987 <farrukh.ep@gmail.com>
2022-02-24 21:39:58 +0500

    in)home

--- src/main/java/com/dc/sql/controller/TaxiOlu4aControler.java
+++ src/main/java/com/dc/sql/controller/TaxiOlu4aControler.java
@@ -1,7 +1,9 @@
 package com.dc.sql.controller;
 
+import com.dc.sql.client.Client;
 import com.dc.sql.service.TaxiOlu4aService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 import javax.net.ssl.SSLException;
+import java.time.Duration;
 
 @RestController
-@RequestMapping("/drivers")
+
 public class TaxiOlu4aControler {
     /*@Autowired
     private TaxiOlu4aRepository TaxiOlu4aRepo;*/
@@ -22,9 +25,28 @@ public class TaxiOlu4aControler {
     //@Autowired
     private TaxiOlu4aService taxiOlu4aService;
 
-    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    @ResponseStatus(HttpStatus.OK)
+    //@GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    //@ResponseStatus(HttpStatus.OK)
+    @Value("${proxy.url}")
+    private String proxyUrl;
+    @Value("${proxy.buffer-size:10000000}")
+    private int proxyBufferSize;
+
+    @Autowired
+    Client webClient;
+    @GetMapping("/drivers")
     public Flux<String> findAll() throws SSLException {
-        return taxiOlu4aService.findAll();
+        return webClient.createWebClient(proxyUrl, proxyBufferSize)
+                .post()
+                //.uri(PathINQUIRY_EXCHANGERATE)  //     /uags/api/v1/exchange
+                .contentType(MediaType.APPLICATION_JSON)
+                //.body(BodyInserters.fromValue(request))  // msgID
+                //.header("UPI-JWS",getupijws.getContent() )
+                .retrieve()
+                .bodyToFlux(String.class)
+                .timeout(Duration.ofMillis(60_000));
     }
+    /*public Flux<String> findAll() throws SSLException {
+        return taxiOlu4aService.findAll();
+    }*/
 }
--- src/main/java/com/dc/sql/service/TaxiOlu4aService.java
+++ src/main/java/com/dc/sql/service/TaxiOlu4aService.java
@@ -3,6 +3,7 @@ package com.dc.sql.service;
 import com.dc.sql.client.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import reactor.core.publisher.Flux;
 
 import javax.net.ssl.SSLException;
@@ -21,9 +22,9 @@ public class TaxiOlu4aService {
 
     public Flux<String> findAll() throws SSLException {
         return webClient.createWebClient(proxyUrl, proxyBufferSize)
-                .get()
+                .post()
                 //.uri(PathINQUIRY_EXCHANGERATE)  //     /uags/api/v1/exchange
-                //.contentType(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
                 //.body(BodyInserters.fromValue(request))  // msgID
                 //.header("UPI-JWS",getupijws.getContent() )
                 .retrieve()
